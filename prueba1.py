de2315ec590f45eba72dda3871e090b087cc4fda
Fazt <contact@faztweb.com>
2022-11-13 11:17:58 -0500

    Los perros no cantan, solo ladran

--- prueba1.py
+++ prueba1.py
@@ -1,1 +1,9 @@
+""" git add nombre.archivo """
+""" git init """
 print("Hola mundo")
+""" git status """
+
+"""   git config --global user.email "you@example.com"
+  git config --global user.name "Your Name" """
+
+  
